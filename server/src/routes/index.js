5a6f5059b71fe69b107458a16a5e8c0b1a426adc
Justin <lehotrongtin123yuanze@gmail.com>
2025-12-29 00:39:09 +0800

    index.js fix again

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,5 @@
-import { Elysia } from 'elysia'
-import { authRoutes } from './auth'
-import { questionRoutes } from './questions'
-import { testRoutes } from './test'
-import { chatbotRoutes } from './chatbot'
-
-const router = (jwt) => new Elysia()
-    .use(authRoutes(jwt))
-    .use(questionRoutes(jwt))
-    .use(testRoutes(jwt))
-    .use(chatbotRoutes(jwt))
-
-export default router
+export { authRoutes } from './auth.js'
+export { questionRoutes } from './questions.js'
+export { testRoutes } from './test.js'
+export { chatbotRoutes } from './chatbot.js'
+export { userRoutes } from './user.js'
